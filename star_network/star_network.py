0132d92eee47f959822f8704439c38af24e5c21c
yamauk <data.googolplex@gmail.com>
2015-05-30 18:29:21 +0900

    Remove drawing network

--- star_network/star_network.py
+++ star_network/star_network.py
@@ -1,7 +1,5 @@
 import redis
 import networkx as nx
-import matplotlib.pyplot as plt
-import pygraphviz as pgv
 
 if __name__ == '__main__':
     red = redis.Redis(host='127.0.0.1', port=6379, db=0)
@@ -14,23 +12,7 @@ if __name__ == '__main__':
         for k, v in nodes.items():
             G.add_edge(k, key, weight=v)
 
-    pos = nx.spring_layout(G)
-    nx.draw_networkx_nodes(G, pos, node_size=19)
-    nx.draw_networkx_edges(G, pos, alpha=0.5)
-    nx.draw_networkx_labels(G, pos, font_size=10, font_color="g")
     pagerank = nx.pagerank_scipy(G, alpha=0.9)
     for k, v in sorted(pagerank.items(), key=lambda x: x[1], reverse=True):
         print  k, v
 
-    # A=nx.to_agraph(G)
-    # A.node_attr['shape']='circle'
-    # A.node_attr['style']='filled'
-    # A.node_attr['width']='0.05'
-    # A.node_attr['label']=''
-    # A.node_attr['fixedsize']=True
-    #
-    # A.layout(prog='neato')
-    # A.draw('file.png')
-
-    plt.axis('off')
-    # plt.show()
